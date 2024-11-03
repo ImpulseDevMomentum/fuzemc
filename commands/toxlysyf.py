725979505717412e39d84379f8d92e713d13de5e
lis0ownik <97831192+lis0ownik@users.noreply.github.com>
2024-11-03 21:59:22 +0100

    Update toxlysyf.py

--- commands/toxlysyf.py
+++ commands/toxlysyf.py
@@ -7,28 +7,28 @@ class FuzeMCCommands(commands.Cog):
 
     @nextcord.slash_command(name="ip", description="Responds with the server IP")
     async def ip(self, interaction: nextcord.Interaction):
-        raniplist = ["fuzemc.pl", "fuzemc.net", "fuzemc.eu"]
+        raniplist = ["ip1", "ip2", "ip3"]
         ranip = random.choice(raniplist)
         await interaction.response.send_message(f"IP: {ranip}", ephemeral=True)
 
     @nextcord.slash_command(name="version", description="Responds with the server version range")
     async def version(self, interaction: nextcord.Interaction):
-        await interaction.response.send_message("Supported versions: 1.8.8 - 1.21.1", ephemeral=True)
+        await interaction.response.send_message("Supported versions: x.x.x - x.x.x", ephemeral=True)
 
     @nextcord.slash_command(name="help", description="Provides a help link")
     async def help(self, interaction: nextcord.Interaction):
         await interaction.response.send_message(
-            "Help: https://discord.com/channels/1283713732387799060/1283728453383753741/1284448855756374078", 
+            "Help: link", 
             ephemeral=True
         )
 
     @nextcord.slash_command(name="shop", description="Redirects to the shop")
     async def shop(self, interaction: nextcord.Interaction):
-        await interaction.response.send_message("Visit our shop: https://fuzemc.net", ephemeral=True)
+        await interaction.response.send_message("Visit our shop: link", ephemeral=True)
 
     @nextcord.slash_command(name="itemshop", description="Redirects to the shop")
     async def itemshop(self, interaction: nextcord.Interaction):
-        await interaction.response.send_message("Visit our shop: https://fuzemc.net", ephemeral=True)
+        await interaction.response.send_message("Visit our shop: link", ephemeral=True)
 
 def setup(bot):
     bot.add_cog(FuzeMCCommands(bot))
